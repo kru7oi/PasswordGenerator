c4290c60db12cb5c7e744c6968550ba9d88bbdd2
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-23 14:55:58 +0300

    Добавлены файлы проекта.

--- PasswordGenerator/AppData/GenerationService.cs
+++ PasswordGenerator/AppData/GenerationService.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordGenerator.AppData
 {
-    internal class GenerationService
+    public class GenerationService
     {
+        private readonly string _numbers = "1234567890";
+        private readonly string _symbols = "!#$%&()*+-_";
+        private readonly string _lowerCharacters = "qwertyuiopasdfghjklzxcvbnm";
+        private readonly string _upperCharacters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        private readonly string[] _words = { "cat", "dog", "mouse", "elephant" };
+        private readonly Random random = new Random();
+        public GenerationService(int length, int numberOfPasswords, bool hasNumbers, bool hasSymbols, bool isUpper, bool isLower, bool hasWords)
+        {
+            Length = length;
+            HasNumbers = hasNumbers;
+            HasSymbols = hasSymbols;
+            IsUpper = isUpper;
+            IsLower = isLower;
+            HasWords = hasWords;
+        }
+
+        public int Length { get; private set; }
+        public int NumberOfPasswords { get; private set; }
+        public bool HasNumbers { get; private set; }
+        public bool HasSymbols { get; private set; }
+        public bool IsUpper { get; private set; }
+        public bool IsLower { get; private set; }
+        public bool HasWords { get; private set; }
+
+        public List<string> Start()
+        {
+            string password = string.Empty;
+            List<string> patterns = new List<string>() { _numbers, _symbols, _lowerCharacters, _upperCharacters };
+            List<string> passwords = new List<string>();
+
+
+
+
+
+            return passwords;
+        }
     }
 }
