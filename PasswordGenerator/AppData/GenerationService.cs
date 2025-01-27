63fcecaedff570df94301d90ec46ee260d312830
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-01-27 12:30:36 +0300

    Внесены изменения.

--- PasswordGenerator/AppData/GenerationService.cs
+++ PasswordGenerator/AppData/GenerationService.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordGenerator.AppData
 {
-    internal class GenerationService
+    public class GenerationService
     {
+        private readonly Random _random = new Random();
+        private readonly string _numbers = "1234567890";
+        private readonly string _symbols = "!@#$%^&*()-_";
+        private readonly string _lowerCharacters = "qwertyuiopasdfghjklzxcvbnm";
+        private readonly string _upperCharacters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+        public List<string> Start(int length)
+        {
+            string[] patterns = new string[] { _numbers, _symbols, _lowerCharacters, _upperCharacters };
+            string password = string.Empty;
+            var passwordSets = new List<string>();
+
+            while (password.Length < length)
+            {
+                int patternIndex = _random.Next(0, patterns.Length);
+                int charIndexFromPattern = _random.Next(0, patterns[patternIndex].Length);
+
+                password += patterns[patternIndex][charIndexFromPattern];
+            }
+
+            passwordSets.Add(password);
+
+            return passwordSets;
+        }
     }
 }
--- PasswordGenerator/View/Windows/MainWindow.xaml.cs
+++ PasswordGenerator/View/Windows/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PasswordGenerator.AppData;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PasswordGenerator
 {
@@ -20,9 +8,18 @@ namespace PasswordGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        GenerationService _generationService;
         public MainWindow()
         {
             InitializeComponent();
+
+            _generationService = new GenerationService();
+        }
+
+        private void GenerateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int length = int.Parse(PasswordLengthTb.Text);
+            PasswordsLb.ItemsSource = _generationService.Start(length);
         }
     }
 }
