0c7185aa70bbce156b06069f3a0bf24deb3aa61e
Vladislav Otdelyanov <vlad_1995@mail.ru>
2025-02-10 09:19:01 +0300

    Создан класс для генерации пароля.

--- PasswordGenerator/AppData/GenerationService.cs
+++ PasswordGenerator/AppData/GenerationService.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordGenerator.AppData
 {
-    internal class GenerationService
+    public class GenerationService
     {
+        private const string UPPER_CASE = "QWERTYUIOPASDFGHJKKLZXCVBNM";
+        private const string LOWER_CASE = "qwertyuiopasdfghjklzxcvbnm";
+        private const string NUMBERS = "0123456789";
+        private const string SEPARATORS = "-_";
+        private const string SYMBOLS = "!@#$%^&*()";
+
+        private readonly string pattern;
+
+        public GenerationService(bool useUpperCase, bool useLowerCase, bool useNumbers, bool useSeparators, bool useSymbols)
+        {
+            if (useUpperCase) pattern += UPPER_CASE;
+            if (useLowerCase) pattern += LOWER_CASE;
+            if (useNumbers) pattern += NUMBERS;
+            if (useSeparators) pattern += SEPARATORS;
+            if (useSymbols) pattern += SYMBOLS;
+        }
+
+        public List<string> GeneratePassword(int length, int countOfPassword)
+        {
+            List<string> passwords = new List<string>();
+            string password = string.Empty;
+
+            Random random = new Random();
+
+            // Реализовать кол-во повторений в зависимости от countOfPassword.
+
+            for (int i = 0; i < length; i++)
+            {
+                int characterIndex = random.Next(pattern.Length);
+                password += pattern[characterIndex];
+            }
+
+            passwords.Add(password);
+
+            return passwords;
+        }
     }
 }
